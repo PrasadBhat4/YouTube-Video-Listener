a3328df8e63c5758568468b20cc08899c4686e69
PrasadBhat4 <40450109+PrasadBhat4@users.noreply.github.com>
2019-11-03 00:00:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    this.onTermSubmit("chakravarthy sulibele");
+    this.onTermSubmit("Javascript medium");
   }
   render() {
     return (
